12f993375e30e90b96a1be21479eae0f324dafa0
Mattia Tupone <tupone.mattia@gmail.com>
2017-11-06 20:05:12 +0100

    Added script for automatic installation

---
+++ rpi-docker-elk-installation.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#rpi-docker-elk stack scripted installation
+#Installing required packages
+sudo apt-get update
+sudo apt-get install -y \
+     apt-transport-https \
+     ca-certificates \
+     curl \
+     python-software-properties
+
+#Adding Docker's official GPG key
+curl -fsSL https://download.docker.com/linux/$(. /etc/os-release; echo "$ID")/gpg | sudo apt-key add -
+
+#Setup stable repository
+echo "deb [arch=armhf] https://download.docker.com/linux/$(. /etc/os-release; echo "$ID") \
+     $(lsb_release -cs) stable" | \
+    sudo tee /etc/apt/sources.list.d/docker.list
+
+#Installing Docker
+sudo apt-get update
+sudo apt-get install -y docker-ce
+
+#Installing pip
+sudo apt install python-software-properties
+
+#Installing Docker compose
+sudo pip install docker-compose
+
+#Starting ELK stack
+docker-compose up
